eae314534e20c20502b4f13d6e85344ef029a055
Saurabh Saneja <saurabh250183@gmail.com>
2024-06-28 05:08:10 -0700

    minor

--- App.tsx
+++ App.tsx
@@ -5,114 +5,47 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import AnimatedSvgIcons from './src/AnimatedSvgIcons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
+  const [isButtonPressed, setIsButtonPressed] = useState(false)
+  const [isApiSuccessful, setIsApiSuccessful] = useState(false)
+  const onPress = () => {
+    setTimeout(() => {
+      setIsButtonPressed(true)
+      setTimeout(() => {
+        setIsApiSuccessful(true)
+      }, 1000);
+    }, 1000);
+  }
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        contentContainerStyle={{ backgroundColor: 'white', flex: 1 }}>
+        <TouchableOpacity onPress={onPress} style={styles.button} >
+          {isApiSuccessful ? <AnimatedSvgIcons /> :
+            isButtonPressed ? <ActivityIndicator color='black' size='small' /> : <Text style={styles.text}>Press this button</Text>}
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  button: { borderWidth: 1, borderColor: 'black', height: 100, alignItems: 'center', justifyContent: 'center' },
+  text: { color: 'black', fontSize: 20 }
 });
 
 export default App;
---
+++ src/AnimatedSvgIcons.js
@@ -0,0 +1,52 @@
+// AnimatedSvgIcons.js
+import React, { useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
+import Animated, { useSharedValue, useAnimatedStyle, withTiming, Easing } from 'react-native-reanimated';
+import CheckBoxGreen from './assets/CheckBoxGreen.svg'
+import CheckBoxWhite from './assets/CheckBoxWhite.svg'
+const AnimatedSvg = Animated.createAnimatedComponent(Svg);
+
+const AnimatedSvgIcons = ({ }) => {
+  const offset = useSharedValue(0);
+  const opacity = useSharedValue(1);
+
+  useEffect(() => {
+    offset.value = withTiming(-100, { duration: 1000, easing: Easing.inOut(Easing.ease) });
+    opacity.value = withTiming(0, { duration: 1000, easing: Easing.inOut(Easing.ease) }, () => {
+      offset.value = 0;
+      opacity.value = 1;
+    });
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: offset.value }],
+      opacity: opacity.value,
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <AnimatedSvg width={100} height={100} style={[styles.icon, animatedStyle]}>
+        <CheckBoxWhite />
+      </AnimatedSvg>
+      <AnimatedSvg width={100} height={100} style={[styles.icon, { opacity: 1 - opacity.value }]}>
+        <CheckBoxGreen />
+      </AnimatedSvg>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    position: 'absolute',
+  },
+});
+
+export default AnimatedSvgIcons;
